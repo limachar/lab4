91905a9ce59c0b982d35df52ff2257c63f127329
limachar <linn.mann@gmail.com>
2020-11-06 19:51:51 +0100

    implemented eeprom function for writing and reading one byte at a time. Also imptemented the write page function to write 8 bytes into a buffer om the eeprom

--- i2c.c
+++ i2c.c
@@ -11,7 +11,7 @@ void i2c_init(void) {
     TWSR = 0x00;
     TWBR = 0x48; 
 
-    TWCR = (1 << TWEN);
+    TWCR = (1 << TWEN);/*set this to 1 to mske pins into i2c interface*/
 }
 
 void i2c_meaningful_status(uint8_t status) {
@@ -39,10 +39,10 @@ void i2c_meaningful_status(uint8_t status) {
 			printf_P(PSTR("MT DATA+W, NAK\n"));
 			break;
 		// MASTER RECEIVE
-		case 0x40: // SLA+R transmitted, ACK received
+		case 0x40: // SLA+R recieved, ACK received
 			printf_P(PSTR("MR SLA+R, ACK\n"));
 			break;
-		case 0x48: // SLA+R transmitted, NAK received
+		case 0x48: // SLA+R recieved, NAK received
 			printf_P(PSTR("MR SLA+R, NAK\n"));
 			break;
 		case 0x50: // DATA received, ACK sent
@@ -58,14 +58,14 @@ void i2c_meaningful_status(uint8_t status) {
 }
 
 inline void i2c_start() {
-	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);/*makes shit happen. TWIE for interrupt, TWEN for enable, TWSTO stop cond, TWSTA start cond, TWEA enable ack, TWINT interrupt flag.*/ 
 
 	while(!(TWCR & (1<<TWINT)));
 }
 
 inline void i2c_stop() {
-	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
-	while(!(TWCR & (1<<TWSTO)));
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);/*SDA low, SCL high, then SDA high*/
+	
 }
 
 inline uint8_t i2c_get_status(void) {
@@ -74,11 +74,12 @@ inline uint8_t i2c_get_status(void) {
 	return status;
 }
 
-inline void i2c_xmit_addr(uint8_t address, uint8_t rw) {
-	TWDR = address;
+inline void i2c_xmit_addr(uint8_t address, uint8_t rw) { /*sending byte*/
+	TWDR = address | rw;/*adress is bit 1-7, bit 0 is r/w. to TWDR you just write the value of the byte you want to send*/
 	TWCR = (1<<TWINT)|(1<<TWEN);
 
 	while(!(TWCR & (1<<TWINT)));
+	
 }
 
 inline void i2c_xmit_byte(uint8_t data) {
@@ -88,9 +89,9 @@ inline void i2c_xmit_byte(uint8_t data) {
 }
 
 inline uint8_t i2c_read_ACK() {
-	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);/*TWEA is the acknoledgement enable bit*/
 	while(!(TWCR & (1<<TWINT)));
-	return TWDR;
+	return TWDR;/*here we recieve a byte*/
 }
 
 inline uint8_t i2c_read_NAK() {
@@ -99,24 +100,103 @@ inline uint8_t i2c_read_NAK() {
 	return TWDR;
 }
 
-inline void eeprom_wait_until_write_complete() {
-	// ...
+inline void eeprom_wait_until_write_complete() {/*if slave does not send ack start again*/
+	while(i2c_get_status() != 0x18){
+
+	i2c_start();
+
+	TWDR = (0xA0 | 0);
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while(!(TWCR & (1<<TWINT)));
+
+	}
 }
 
-uint8_t eeprom_read_byte(uint8_t addr) {
-	// ...
+uint8_t eeprom_read_byte(uint8_t addr) {/*8.2 in the sheet*/
+	uint8_t byte_read = 0;
+	i2c_start();
+
+	i2c_xmit_addr(0xA0, 0);
+
+	i2c_xmit_byte(addr);
+
+	i2c_start();
+
+	i2c_xmit_addr(0xA0, 1);
+
+	byte_read = i2c_read_NAK();/*sending a nack to tell slave im done recieving*/
+
+	i2c_stop();
+
+	return byte_read;
 }
 
-void eeprom_write_byte(uint8_t addr, uint8_t data) {
-	// ...
+void eeprom_write_byte(uint8_t addr, uint8_t data) {/*6.1 in the sheet*/
+
+	i2c_start();
+
+	i2c_xmit_addr(0xA0, 0);
+	
+	i2c_xmit_byte(addr);
+
+	i2c_xmit_byte(data);
+
+	i2c_stop();
+
+	eeprom_wait_until_write_complete();/*make sure the write is complete*/
 }
 
 
 
 void eeprom_write_page(uint8_t addr, uint8_t *data) {
-	// ... (VG)
+	
+	while(addr % 8 != 0){ 
+		addr++;
+	}
+		
+	int i = 0;
+
+	i2c_start();
+
+	i2c_xmit_addr(0xA0, 0);
+
+	
+	i2c_xmit_byte(addr);
+
+	for(i = 0; i<8; i++){ 	/*writing only 8 bits*/
+	i2c_xmit_byte(data[i]);
+	}
+
+	i2c_stop();
+
+	eeprom_wait_until_write_complete();
 }
 
 void eeprom_sequential_read(uint8_t *buf, uint8_t start_addr, uint8_t len) {
-	// ... (VG)
+	
+	
+	while(start_addr % 8 != 0){
+		start_addr++;
+	}
+	
+	int i;
+
+	i2c_start();
+
+	i2c_xmit_addr(0xA0, 0);
+	
+	i2c_xmit_byte(start_addr);
+
+	i2c_start();
+
+	i2c_xmit_addr(0xA0, 1);
+
+	for(i = 0; i < (len-1); i++){
+	buf[i] = i2c_read_ACK();/*sending ack so slave knows i what to read more*/
+	}
+
+	buf[len-1] = i2c_read_NAK();
+
+	i2c_stop();
+
 }
--- main.c
+++ main.c
@@ -10,6 +10,13 @@
 #include "i2c.h"
 #include "serial.h"
 #include "timer.h"
+uint8_t address = 0x30;
+uint8_t rw = 0;
+uint8_t data = 'L';
+char name[] = "Linn";
+uint8_t i = 0;
+char buffer[10];
+
 
 
 void main (void) {
@@ -18,9 +25,22 @@ void main (void) {
 	uart_init();
 
 	sei();
+	/*for(i = 0; i<sizeof(name); i++){
+	eeprom_write_byte((address+i),name[i]);
+	}
+	uint8_t bytees = eeprom_read_byte(0x21);
+	*/
 
+	
+
+	eeprom_write_page(0x30, name);
+	
+	
+	
 	while (1) {
-
+		eeprom_sequential_read(buffer, 0x30, sizeof(name));
+		printf_P(PSTR("%s\n"), buffer);
+        
 	}
 }
 
